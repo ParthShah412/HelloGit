dac692c067dc66c8045aaf294329fc32d5c2725c
ParthShah <pinkushah05@gmail.com>
2018-01-25 19:18:06 +0530

    A new File added

---
+++ HelloGit/src/HelloMiddleEarth.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class HelloMiddleEarth{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String fav_chr;
+		System.out.print("Enter your Favourite Middle Earth Character:- ");
+		fav_chr = scan.nextLine();
+		System.out.println("Your Favourite Middle Earth Character is:- "+fav_chr);
+		
+	}
+}
